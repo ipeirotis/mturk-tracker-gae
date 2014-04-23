db4c8c6526d0c2ccff391faee436f520cfb83fd9
Panos Ipeirotis <ipeirotis@gmail.com>
2014-04-23 11:02:26 -0400

    Fixed bug with arrival completions

--- src/main/java/com/tracker/servlet/ComputeArrivalCompletionsServlet.java
+++ src/main/java/com/tracker/servlet/ComputeArrivalCompletionsServlet.java
@@ -26,26 +26,48 @@ public class ComputeArrivalCompletionsServlet extends HttpServlet {
   public void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws IOException {
     String schedule = req.getParameter("schedule");
+    
+    String to_param = req.getParameter("to");
+    Calendar to = getToTime(to_param);
+    
+    String from_param = req.getParameter("from");
+    Calendar from = getFromTime(to, from_param);
+    
+    
     if("true".equals(schedule)){
-      schedule();
+      schedule(from, to);
       return;
     } else {
-      compute();
+      compute(from, to);
     }
   }
-  
-  private void compute(){
+
+  private Calendar getFromTime(Calendar to, String from_param) {
     Calendar from = Calendar.getInstance();
-    from.setTime(new Date());
-    from.set(Calendar.MINUTE, 0);
-    from.set(Calendar.SECOND, 0);
-    from.set(Calendar.MILLISECOND, 0);
     
+    if (from_param==null) {
+      from.setTimeInMillis(to.getTimeInMillis());
+      from.add(Calendar.HOUR, -1);
+    } else {
+      Long from_timestamp = Long.parseLong(from_param);
+      from.setTimeInMillis(from_timestamp);
+    }
+    return from;
+  }
+
+  private Calendar getToTime(String to_param) {
     Calendar to = Calendar.getInstance();
-    to.setTime(new Date());
-    to.set(Calendar.MINUTE, 59);
-    to.set(Calendar.SECOND, 59);
-    from.set(Calendar.MILLISECOND, 999);
+    
+    if (to_param==null) {
+      to.setTime(new Date());
+    } else {
+      Long to_timestamp = Long.parseLong(to_param);
+      to.setTimeInMillis(to_timestamp);
+    }
+    return to;
+  }
+  
+  private void compute(Calendar from, Calendar to){
     
     List<HITgroup> arrivedGroups = ofy().load().type(HITgroup.class)
         .filter("firstSeen >", from.getTime()).filter("firstSeen <", to.getTime()).list();
@@ -86,10 +108,12 @@ public class ComputeArrivalCompletionsServlet extends HttpServlet {
         hitGroupsCompleted, hitsArrived, hitsCompleted, rewardsArrived, rewardsCompleted));
   }
 
-  private void schedule(){
+  private void schedule(Calendar from, Calendar to){
     Queue queue = QueueFactory.getDefaultQueue();
     queue.add(Builder
         .withUrl("/computeArrivalCompletions")
+        .param("from", Long.toString(from.getTimeInMillis()))
+        .param("to", Long.toString(to.getTimeInMillis()))
         .etaMillis(System.currentTimeMillis())
         .retryOptions(RetryOptions.Builder.withTaskRetryLimit(1))
         .method(TaskOptions.Method.GET));
