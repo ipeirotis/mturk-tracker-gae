af0614f082330de21ac58c10c9b884e77207da39
Panos Ipeirotis <ipeirotis@gmail.com>
2014-05-01 11:12:52 -0400

    Fixing the logic of the TrackHITs: Update the lastSeen only when we can still find the HIT alive. Do not store HITinstance when nothing has changed.

--- src/main/java/com/tracker/servlet/TrackHITsServlet.java
+++ src/main/java/com/tracker/servlet/TrackHITsServlet.java
@@ -25,56 +25,75 @@ import com.tracker.entity.HITinstance;
 
 @SuppressWarnings("serial")
 public class TrackHITsServlet extends HttpServlet {
-  
+
   @SuppressWarnings("unused")
-private static final Logger logger = Logger.getLogger(TrackHITsServlet.class.getName());
+  private static final Logger logger = Logger.getLogger(TrackHITsServlet.class.getName());
   private static final String PREVIEW_URL = "https://www.mturk.com/mturk/preview?groupId=";
   private static final String NOT_AVAILABLE_ALERT = "There are no more available HITs in this group";
-  
+
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-	  List<HITgroup> groups = ofy().load().type(HITgroup.class).filter("active", true).list();
 
-	  for(HITgroup group : groups){
-		  schedule(group.getGroupId());
-	  }
+    // Get the list of all HITgroups that are active.
+    List<HITgroup> groups = ofy().load().type(HITgroup.class).filter("active", true).list();
+    for (HITgroup group : groups) {
+      schedule(group.getGroupId());
+    }
   }
-  
+
   public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-	  loadAndParse(req.getParameter("groupId"));
+    loadAndParse(req.getParameter("groupId"));
   }
-  
-  private void loadAndParse(String groupId) throws IOException{
-	  HITgroup group = ofy().load().type(HITgroup.class).id(groupId).now();
 
-	  Document doc = Jsoup.connect(PREVIEW_URL + group.getGroupId()).get();
-	  Element alertboxHeader = doc.getElementById("alertboxHeader");
-	  if(alertboxHeader != null && alertboxHeader.text().startsWith(NOT_AVAILABLE_ALERT)){
-		  group.setActive(false);
-		  ofy().save().entity(group);
-		  ofy().save().entity(new HITinstance(group.getGroupId(), new Date(), 0, 0, 0, 0));
-	  } else {
-		  Element hitElement = doc.select("a:matchesOwn(HITs Available:+)").first();
-		  String hits = hitElement.parent().nextElementSibling().text();
-		  Integer iHits = Integer.parseInt(hits);
+  private void loadAndParse(String groupId) throws IOException {
+    HITgroup group = ofy().load().type(HITgroup.class).id(groupId).now();
+    HITinstance recentInstance = getRecentInstance(group.getGroupId());
+    Integer recentHITsAvailable = (recentInstance == null) ? 0 : recentInstance.getHitsAvailable();
+    Integer recentRewardsAvailable = recentHITsAvailable * group.getReward();
 
-		  HITinstance recentInstance = getRecentInstance(group.getGroupId());
-		  Integer hitsDiff = recentInstance == null ? 0 : (iHits-recentInstance.getHitsAvailable());
-		  Integer rewardsDiff = recentInstance == null ? 0 : (iHits-recentInstance.getHitsAvailable())*group.getReward();
-		  ofy().save().entity(new HITinstance(group.getGroupId(), new Date(), iHits, hitsDiff, group.getReward()*iHits, rewardsDiff));
+    Document doc = Jsoup.connect(PREVIEW_URL + group.getGroupId()).get();
+    Element alertboxHeader = doc.getElementById("alertboxHeader");
 
-		  if(recentInstance != null){
-			  group.setLastSeen(new Date());
-			  ofy().save().entity(group);
-		  }
-	  }
+    if (alertboxHeader != null && alertboxHeader.text().startsWith(NOT_AVAILABLE_ALERT)) {
+      // We cannot find the HIT anymore. So we set the "active" flag for the
+      // HITgroup to false and create a final HIT instance that shows that we have 0 HITs
+      // available (this is necessary to have correct values for the ArrivalCompletions
+      // computation)
+      group.setActive(false);
+      ofy().save().entity(group);
+
+      Integer hitsDiff = 0 - recentHITsAvailable;
+      Integer rewardsDiff = 0 - recentRewardsAvailable;
+      HITinstance hitinstance = new HITinstance(group.getGroupId(), new Date(), 0, hitsDiff, 0, rewardsDiff);
+      ofy().save().entity(hitinstance);
+    } else {
+      // We found the HIT on the system, so we update the lastSeen variable for
+      // the HITgroup
+      group.setLastSeen(new Date());
+      ofy().save().entity(group);
+
+      Element hitElement = doc.select("a:matchesOwn(HITs Available:+)").first();
+      String hits = hitElement.parent().nextElementSibling().text();
+      Integer iHits = Integer.parseInt(hits);
+
+      Integer hitsDiff = iHits - recentHITsAvailable;
+      Integer rewardsDiff = hitsDiff * group.getReward();
+
+      if (hitsDiff != 0) {
+        // We only save a new HIT instance if there is a change compared to the
+        // prior HITinstance
+        HITinstance hitinstance = new HITinstance(group.getGroupId(), new Date(), iHits, hitsDiff, group.getReward() * iHits,
+            rewardsDiff);
+        ofy().save().entity(hitinstance);
+      }
+
+    }
   }
-  
-  private HITinstance getRecentInstance(String groupId){
-    return ofy().load().type(HITinstance.class)
-        .filter("groupId", groupId).order("-timestamp").limit(1).first().now();
+
+  private HITinstance getRecentInstance(String groupId) {
+    return ofy().load().type(HITinstance.class).filter("groupId", groupId).order("-timestamp").limit(1).first().now();
   }
-  
-  private void schedule(String groupId){
+
+  private void schedule(String groupId) {
     Queue queue = QueueFactory.getDefaultQueue();
     queue.add(Builder
         .withUrl("/trackHits")
