5933cc7f788a778e133602a68d29070997975757
Panos Ipeirotis <ipeirotis@gmail.com>
2014-05-05 16:47:23 -0400

    Fixing some bugs at the front end

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -2,35 +2,42 @@ google.load('visualization', '1.1', {
 	'packages' : [ 'annotationchart' ]
 });
 
+
+
+
+
+
+function drawAnnotationChart(chart, data, options) {
+	chart.draw(data, options);
+}
+
+
+
+
 $(function() {
 	google.setOnLoadCallback(drawChart);
 	
+	var msChart = new google.visualization.AnnotationChart(document.getElementById('marketStatsChart'));
+	var msChartData = new google.visualization.DataTable();
+	msChartData.addColumn('datetime', 'Date');
+	msChartData.addColumn('number', 'HITs available');
+	msChartData.addColumn('number', 'HIT groups available');
 
+	var groupsChart = new google.visualization.AnnotationChart(document.getElementById('groupsChart'));
+	var groupsChartData = new google.visualization.DataTable();
+	groupsChartData.addColumn('datetime', 'Date');
+	groupsChartData.addColumn('number', 'HIT groups posted');
 
-	var activeTab = '#marketStats';
+	var hitsChart = new google.visualization.AnnotationChart(document.getElementById('hitsChart'));
+	var hitsChartData = new google.visualization.DataTable();
+	hitsChartData.addColumn('datetime', 'Date');
+	hitsChartData.addColumn('number', 'HITs posted');
 
-	$('#chartsTab a').click(function(e) {
-		e.preventDefault();
-		$(this).tab('show');
-		activeTab = $(this).attr('href');
-
-		if ($(this).attr('href') == '#marketStats') {
-			drawMsChart();
-		}
-
-		if ($(this).attr('href') == '#rewards') {
-			drawRewardsChart();
-		}
-
-		if ($(this).attr('href') == '#hits') {
-			drawHitsChart();
-		}
-
-		if ($(this).attr('href') == '#groups') {
-			drawGroupsChart();
-		}
-	});
-
+	var rewardsChart = new google.visualization.AnnotationChart(document.getElementById('rewardsChart'));
+	var rewardsChartData = new google.visualization.DataTable();
+	rewardsChartData.addColumn('datetime', 'Date');
+	rewardsChartData.addColumn('number', 'Rewards posted');
+	
 	$.datepicker.setDefaults($.extend({
 		dateFormat : 'mm/dd/yy',
 		showMonthAfterYear : false
@@ -50,16 +57,23 @@ $(function() {
 	$('#date_to').datepicker('setDate', new Date());
 	$('#date_from').datepicker('option', 'maxDate', maxDate);
 	$('#date_to').datepicker('option', 'maxDate', maxDate);
-	
-	
+
 
 	$('#date_from').on('change', function() {
+		fillStatisticsData(host, protocol, null);
+		fillArrivalData(host, protocol);
+		
 		drawChart();
 	});
 
 	$('#date_to').on('change', function() {
+		fillStatisticsData(host, protocol, null);
+		fillArrivalData(host, protocol);
+		
 		drawChart();
 	});
+	
+	var activeTab = '#rewards';
 
 	// charts
 	var options = {
@@ -67,134 +81,89 @@ $(function() {
 		scaleType : 'allmaximized',
 		scaleColumns : [ 0, 1 ]
 	};
+	
+	$('#chartsTab a').click(function(e) {
+		e.preventDefault();
+		$(this).tab('show');
+		activeTab = $(this).attr('href');
 
-	var msChart;// marketStatistics chart
-	var msChartData = new google.visualization.DataTable();
-	msChartData.addColumn('datetime', 'Date');
-	msChartData.addColumn('number', 'HITs available');
-	msChartData.addColumn('number', 'HIT groups available');
+		drawChart();
+	});
 
-	var groupsChart;
-	var groupsChartData = new google.visualization.DataTable();
-	groupsChartData.addColumn('datetime', 'Date');
-	groupsChartData.addColumn('number', 'HIT groups posted');
 
-	var hitsChart;
-	var hitsChartData = new google.visualization.DataTable();
-	hitsChartData.addColumn('datetime', 'Date');
-	hitsChartData.addColumn('number', 'HITs posted');
-
-	var rewardsChart;
-	var rewardsChartData = new google.visualization.DataTable();
-	rewardsChartData.addColumn('datetime', 'Date');
-	rewardsChartData.addColumn('number', 'Rewards posted');
 	
 	var host = window.location.host;
 	var protocol = host.indexOf('localhost', 0) == 0 ? 'http' : 'https';
-	fillArrivalData(host, protocol);
 	fillStatisticsData(host, protocol, null);
-
+	fillArrivalData(host, protocol);
+	
+	drawChart();
 	
 	function fillArrivalData(host, protocol) {
-		var url = protocol + '://' + host
-		+ '/_ah/api/mturk/v1/arrivalCompletions/list?from='
-		+ $("#date_from").val() + '&to=' + $("#date_to").val();
+		var url = protocol + '://' + host + '/_ah/api/mturk/v1/arrivalCompletions/list?from=' + $("#date_from").val() + '&to=' + $("#date_to").val();
 
 		$.ajax({type : 'GET',  url : url, dataType : 'json'})
 			.done(function(response) {
-		
-					if (response.items) {
-						$.each(response.items, function(index, item) {
-							groupsChartData.addRows([ [ new Date(item.from),
-									parseInt(item.hitGroupsArrived) ] ]);
-							hitsChartData.addRows([ [ new Date(item.from),
-									parseInt(item.hitsArrived) ] ]);
-							rewardsChartData.addRows([ [ new Date(item.from),
-									parseInt(item.rewardsArrived) / 100 ] ]);
-		
-						});
-					}
-					drawChart();
-		
-
-				}).fail(function(e) {
-			// TODO
-		});
+				if (response.items) {
+					$.each(response.items, function(index, item) {
+						groupsChartData.addRows([ [ new Date(item.from), parseInt(item.hitGroupsArrived) ] ]);
+						hitsChartData.addRows([ [ new Date(item.from),	parseInt(item.hitsArrived) ] ]);
+						rewardsChartData.addRows([ [ new Date(item.from), parseInt(item.rewardsArrived) / 100 ] ]);
+					});
+				}
+			})
+			.fail(function(e) {
+			})
+			.complete(function(e) {
+				drawChart();
+			})
+			;
 	}
 	
 	function fillStatisticsData(host, protocol, cursor) {
-		var url = protocol + '://' + host
-		+ '/_ah/api/mturk/v1/marketStatistics/list?from='
-		+ $("#date_from").val() + '&to=' + $("#date_to").val();
+		var url = protocol + '://' + host + '/_ah/api/mturk/v1/marketStatistics/list?from='	+ $("#date_from").val() + '&to=' + $("#date_to").val();
 		
 		if (cursor) {
 			url = url +'&cursor=' + cursor;
 		}
 
-			$.ajax({ type : 'GET',	url : url,	dataType : 'json' })
+		$.ajax({ type : 'GET',	url : url,	dataType : 'json' })
 			.done(function(response) {
-
-						if (response.items) {
-							$.each(response.items, function(index, item) {
-								msChartData.addRows([ [new Date(item.timestamp), parseInt(item.hitsAvailable),	parseInt(item.hitGroupsAvailable) ] ]);	});
-						}
-
-						if (response.nextPageToken) {
-							fillStatisticsData(host, protocol, response.nextPageToken);
-						} else{
-							drawChart();
-						}
-
-					}).fail(function(e) {
+				if (response.items) {
+					$.each(response.items, function(index, item) {
+						msChartData.addRows([ [new Date(item.timestamp), parseInt(item.hitsAvailable),	parseInt(item.hitGroupsAvailable) ] ]);	
+					});
+				}
+				if (response.nextPageToken) {
+					fillStatisticsData(host, protocol, response.nextPageToken);
+				} 
+			})
+			.fail(function(e) {
 				// TODO
-			});
-
+			})
+			.complete(function(e) {
+				if (cursor==null) {
+					drawChart();
+				}
+			})
+			;
 	}
 	
 	function drawChart() {
 
 		if (activeTab == '#marketStats') {
-			drawMsChart();
+			drawAnnotationChart(msChart, msChartData, options);
 		} else if (activeTab == '#hits') {
-			drawHitsChart();
+			drawAnnotationChart(hitsChart, hitsChartData, options);
 		} else if (activeTab == '#rewards') {
-			drawRewardsChart();
+			drawAnnotationChart(rewardsChart, rewardsChartData, options);
 		} else if (activeTab == '#groups') {
-			drawGroupsChart();
+			drawAnnotationChart(groupsChart, groupsChartData, options);
 		}
-		
 
 	}
 
-	function drawMsChart() {
-		if (!msChart) {
-			msChart = new google.visualization.AnnotationChart(document
-					.getElementById('marketStatsChart'));
-		}
-		msChart.draw(msChartData, options);
-	}
 
-	function drawHitsChart() {
-		if (!hitsChart) {
-			hitsChart = new google.visualization.AnnotationChart(document
-					.getElementById('hitsChart'));
-		}
-		hitsChart.draw(hitsChartData, options);
-	}
-
-	function drawGroupsChart() {
-		if (!groupsChart) {
-			groupsChart = new google.visualization.AnnotationChart(document
-					.getElementById('groupsChart'));
-		}
-		groupsChart.draw(groupsChartData, options);
-	}
-
-	function drawRewardsChart() {
-		if (!rewardsChart) {
-			rewardsChart = new google.visualization.AnnotationChart(document
-					.getElementById('rewardsChart'));
-		}
-		rewardsChart.draw(rewardsChartData, options);
-	}
 });
+
+
