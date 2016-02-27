42bb91ab622c92395a3129d26095bba678c5e513
nkislitsin <nkislitsin@mail.ru>
2016-02-27 11:56:50 +0300

    daily job for active requesters

--- src/main/java/com/tracker/servlet/task/ComputeActiveRequesters.java
+++ src/main/java/com/tracker/servlet/task/ComputeActiveRequesters.java
@@ -30,57 +30,58 @@ import com.tracker.util.SafeDateFormat;
 @SuppressWarnings("serial")
 public class ComputeActiveRequesters extends HttpServlet {
 
-  private static final Logger logger = Logger.getLogger(ComputeActiveRequesters.class.getName());
+    @SuppressWarnings("unused")
+    private static final Logger logger = Logger.getLogger(ComputeActiveRequesters.class.getName());
 
-  private static final String APPLICATION_ID = SystemProperty.applicationId.get();
-  private static final String BUCKET = "entities";
+    private static final String APPLICATION_ID = SystemProperty.applicationId.get();
+    private static final String BUCKET = "entities";
 
-  private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
-  private static final JsonFactory JSON_FACTORY = new JacksonFactory();
-  
-  private static final DateFormat dateFormat = SafeDateFormat.forPattern("yyyy-MM-dd HH:mm"); 
-  
-  public void doGet(HttpServletRequest req, HttpServletResponse resp)
-    throws IOException {
+    private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+    private static final JsonFactory JSON_FACTORY = new JacksonFactory();
 
-    Calendar dateFrom = Calendar.getInstance();
-    dateFrom.setTime(new Date());
-    dateFrom.set(Calendar.MINUTE, 0);
-    dateFrom.set(Calendar.SECOND, 0);
-    dateFrom.set(Calendar.MILLISECOND, 0);
-    dateFrom.add(Calendar.HOUR_OF_DAY, -1);
+    private static final DateFormat dateFormat = SafeDateFormat.forPattern("yyyy-MM-dd HH:mm");
 
-    Calendar dateTo = Calendar.getInstance();
-    dateTo.setTime(dateFrom.getTime());
-    dateTo.add(Calendar.HOUR_OF_DAY, 1);
-    
-    AppIdentityCredential appIdentityCredential = new AppIdentityCredential(Collections.singleton(BigqueryScopes.BIGQUERY));
-    Bigquery bigquery = new Bigquery.Builder(HTTP_TRANSPORT, JSON_FACTORY, appIdentityCredential)
-      .setApplicationName(APPLICATION_ID)
-      .build();
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
 
-    BigQueryService bigQueryService = new BigQueryService(bigquery, APPLICATION_ID, BUCKET);
+        Calendar dateFrom = Calendar.getInstance();
+        dateFrom.setTime(new Date());
+        dateFrom.set(Calendar.HOUR_OF_DAY, 0);
+        dateFrom.set(Calendar.MINUTE, 0);
+        dateFrom.set(Calendar.SECOND, 0);
+        dateFrom.set(Calendar.MILLISECOND, 0);
+        dateFrom.add(Calendar.DAY_OF_MONTH, -1);
 
-    String query = String.format("SELECT requesterId, COUNT(*) AS cnt "
-            + "FROM [entities.HITgroup] WHERE firstSeen<'%s' AND lastSeen>'%s' GROUP BY requesterId",
-            dateFormat.format(dateFrom.getTime()), dateFormat.format(dateTo.getTime()));
+        Calendar dateTo = Calendar.getInstance();
+        dateTo.setTime(dateFrom.getTime());
+        dateTo.add(Calendar.DAY_OF_MONTH, 1);
 
-    QueryResult queryResult = bigQueryService.executeQuery(query);
-    logger.info(String.format("%d rows fetched", queryResult.getData().size()));
+        AppIdentityCredential appIdentityCredential = new AppIdentityCredential(Collections.singleton(BigqueryScopes.BIGQUERY));
+        Bigquery bigquery = new Bigquery.Builder(HTTP_TRANSPORT, JSON_FACTORY, appIdentityCredential)
+        .setApplicationName(APPLICATION_ID)
+        .build();
 
-    long count = 0;
-    for(List<Object> row : queryResult.getData()){
-        count += Long.parseLong((String)row.get(1));
-    }
+        BigQueryService bigQueryService = new BigQueryService(bigquery, APPLICATION_ID, BUCKET);
 
-    if(count > 0) {
-        ArrivalCompletions arrivalCompletions = ofy().load().type(ArrivalCompletions.class)
-        .filter("from", dateFrom.getTime()).filter("to", dateTo.getTime()).limit(1).first().now();
-        if(arrivalCompletions != null) {
-            arrivalCompletions.setActiveRequesters(count);
-            ofy().save().entity(arrivalCompletions);
+        List<ArrivalCompletions> list = ofy().load().type(ArrivalCompletions.class)
+                .filter("from >=", dateFrom.getTime()).filter("from <", dateTo.getTime()).list();
+
+        for(ArrivalCompletions ac :list) {
+            String query = String.format("SELECT requesterId, COUNT(*) AS cnt "
+                    + "FROM [entities.HITgroup] WHERE firstSeen<'%s' AND lastSeen>'%s' GROUP BY requesterId",
+                    dateFormat.format(ac.getFrom()), dateFormat.format(ac.getTo()));
+
+            QueryResult queryResult = bigQueryService.executeQuery(query);
+
+            long count = 0;
+            for(List<Object> row : queryResult.getData()){
+                count += Long.parseLong((String)row.get(1));
+            }
+
+            if(count > 0) {
+                ac.setActiveRequesters(count);
+                ofy().save().entity(ac);
+            }
         }
     }
-  }
-
 }
